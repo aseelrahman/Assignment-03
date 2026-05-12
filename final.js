3e9b0fcb2259714a8cce68a8803509f11110663a
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-12 09:16:57 -0400

    Assignment 3 problem 01

---
+++ final.js
@@ -0,0 +1,13 @@
+// --------------- Problem - 01 ----------------------
+function newPrice(currentPrice, discount) {
+    if(typeof currentPrice !== 'number' || typeof discount !== 'number' || discount < 0 || discount > 100){
+        return 'Invalid';
+    }
+    else{
+        let discountPrice = (currentPrice * discount)/100;
+        let newPrice = currentPrice - discountPrice;
+        return newPrice.toFixed(3);
+    }
+}
+
+// --------------- Problem - 02 ----------------------
